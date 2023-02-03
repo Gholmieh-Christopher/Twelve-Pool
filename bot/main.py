b1dca6020d4699b9ce814000f8ab42e5f7063400
Christopher Gholmieh <109567418+Gholmieh-Christopher@users.noreply.github.com>
2023-02-03 14:28:55 -0800

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -24,6 +24,7 @@ import typing
 # Loguru:
 import loguru
 
+
 # Classes:
 class TwelvePool(BotAI):
     # Configuration:
@@ -31,13 +32,81 @@ class TwelvePool(BotAI):
 
     RACE: Race = Race.Zerg
 
+    # Methods:
+    async def speedmine_single(self, drone: Unit) -> None:
+        mineral_field: Unit = self.mineral_field.closest_to(drone)
+        if mineral_field is None:
+            return None
+
+        townhall: Unit = self.townhalls.closest_to(drone)
+        if townhall is None:
+            return None
+
+        if drone.is_returning and len(drone.orders) == 1:
+            target: Point2 = townhall.position.towards(
+                drone, townhall.radius + drone.radius
+            )
+
+            if 0.75 < drone.distance_to(target) < 2:
+                drone.move(target)
+                drone(AbilityId.SMART, townhall, queue=True)
+
+                return None
+
+            if (
+                drone.is_returning is False
+                and len(drone.orders) == 1
+                and isinstance(drone.order_target, int)
+            ):
+                if 0.75 < drone.distance_to(townhall) < 2:
+                    drone.move(townhall)
+                    drone(AbilityId.SMART, mineral_field, queue=True)
+
     # Events:
+    async def on_unit_created(self, unit: Unit):
+        if unit.type_id == UnitTypeId.QUEEN:
+            townhall: typing.Optional[Unit] = self.townhalls.closest_to(unit)
+            if townhall is None:
+                self.idle_queens.add(unit.tag)
+            else:
+                if townhall.tag in self.queen_registry:
+                    self.idle_queens.add(unit.tag)
+                else:
+                    self.queen_registry[townhall.tag] = unit.tag
+                    unit(AbilityId.EFFECT_INJECTLARVA, townhall)
+
+    async def on_unit_destroyed(self, unit_tag: int):
+        if unit_tag not in self.queen_registry:
+            return None
+
+        if unit_tag in self.queen_registry.values():
+            del self.queen_registry[unit_tag]
+            pass
+
+        unready_townhalls: Units = self.townhalls.filter(
+            lambda townhall: not townhall.is_ready
+        )
+        if not any(unready_townhalls):
+            self.idle_queens.add(self.queen_registry[unit_tag])
+        else:
+            self.queen_registry[unit_tag] = unready_townhalls.closest_to(
+                self.queen_registry[unit_tag]
+            )
+
+        del self.queen_registry[unit_tag]
+
     async def on_start(self) -> None:
-        pass
+        self.queen_registry: dict = {
+            # NOTE: townhall_id: queen_tag
+        }
+
+        self.idle_queens: set = set()
+        self.idle_clean: set = set()
+
+        self.threshold: int = 20
 
     async def on_step(self, iteration: int) -> None:
         zerglings: Units = self.units.of_type(UnitTypeId.ZERGLING)
-        queens: Units = self.units.of_type(UnitTypeId.QUEEN)
 
         if (
             self.can_afford(UnitTypeId.SPAWNINGPOOL)
@@ -49,20 +118,23 @@ class TwelvePool(BotAI):
                 loguru.logger.info("No drone available to construct spawning pool.")
                 return None
 
-            position: typing.Optional[Point2] = await self.find_placement(UnitTypeId.SPAWNINGPOOL, near=self.townhalls.first.position)
+            position: typing.Optional[Point2] = await self.find_placement(
+                UnitTypeId.SPAWNINGPOOL, near=self.townhalls.first.position
+            )
             if position is None:
                 loguru.logger.info("No position available to construct spawning pool.")
                 return None
 
             drone.build(UnitTypeId.SPAWNINGPOOL, position)
-        
+
         if self.structures.of_type(UnitTypeId.SPAWNINGPOOL).ready.amount == 1:
             for larva in self.larva:
                 if (
                     self.supply_left <= 2
-                    and self.already_pending(UnitTypeId.OVERLORD) == 0 
+                    and self.already_pending(UnitTypeId.OVERLORD) == 0
                     and self.can_afford(UnitTypeId.OVERLORD)
-                    and self.structures.of_type(UnitTypeId.SPAWNINGPOOL).ready.amount == 1
+                    and self.structures.of_type(UnitTypeId.SPAWNINGPOOL).ready.amount
+                    == 1
                 ):
                     larva.train(UnitTypeId.OVERLORD)
 
@@ -72,23 +144,43 @@ class TwelvePool(BotAI):
                     break
 
                 larva.train(UnitTypeId.ZERGLING)
-        
+
         for zergling in zerglings.idle:
             zergling.attack(self.enemy_start_locations[0])
 
-        if (
-            self.can_afford(UnitTypeId.QUEEN)
-            and self.already_pending(UnitTypeId.QUEEN) == 0
-            and self.units.of_type(UnitTypeId.QUEEN).amount == 0
-            and self.structures.of_type(UnitTypeId.SPAWNINGPOOL).ready.amount == 1
-        ):
-            self.townhalls.first.train(UnitTypeId.QUEEN)
-        
-        if (
-            self.minerals >= 500
-        ):
+        if self.minerals >= 500:
             await self.expand_now(UnitTypeId.HATCHERY)
 
-        # TODO: Add speedmining
-        # TODO: Queens per individual hatchery
-        # TODO: Add a mix of drones.
+        for queen in self.idle_queens:
+            available: Units = self.townhalls.filter(
+                lambda townhall: townhall.tag not in self.queen_registry
+            )
+            if not any(available):
+                return None
+            townhall: Unit = available.closest_to(queen)
+
+            self.queen_registry[townhall.tag] = queen.tag
+            if queen.energy >= 25:
+                queen(AbilityId.EFFECT_INJECTLARVA, townhall)
+            else:
+                queen.move(townhall)
+            self.idle_clean.add(queen.tag)
+
+        for tag in self.idle_clean:
+            self.idle_clean.remove(tag)
+
+        self.idle_clean: set = set()
+
+        for hatchery in self.townhalls:
+            if (
+                hatchery.tag not in self.queen_registry
+                and hatchery.is_idle
+                and self.can_afford(UnitTypeId.QUEEN)
+                and self.structures.of_type(UnitTypeId.SPAWNINGPOOL).ready.amount == 1
+            ):
+                hatchery.train(UnitTypeId.QUEEN)
+
+        for worker in self.workers:
+            await self.speedmine_single(worker)
+
+        
